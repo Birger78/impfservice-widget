c3d1f06045881c8c34e3daee20456e880ebf2f5e
Birger Stöckelmann <bist@dcc.ruhr>
2021-03-11 21:34:03 +0100

    [TASK] "Wartungsarbeiten" and "Warteraum" parsing and output

--- Impftermine.js
+++ Impftermine.js
@@ -20,29 +20,58 @@ const dateFormatter = new DateFormatter()
 dateFormatter.dateFormat = 'd. MMMM YYYY, HH:mm:ss'
 
 const getData = async (zip) => {
-  let lstMs = await new Request(vaccineListUrl).loadJSON()
-  let lstMString = lstMs.map(item => item.qualification).join(',')
-  const wv = new WebView()
-  let url = `${serviceUrl}?plz=${zip}`
-  await wv.loadURL(url)
-  await wv.evaluateJavaScript('setTimeout(function(){completion(null)}, 3000)', true)
-  await wv.loadURL(`${requestSlotUrl}?plz=${zip}&leistungsmerkmale=${lstMString}`)
-  let htmlRes = (await wv.getHTML()).replace(/(<([^>]+)>)/gi, '')
-  return JSON.parse(htmlRes)
+
+  let lstMString = ''
+  let jsonResponse = {}
+
+  try {
+
+    // load base site
+    const wv = new WebView()
+    let url = `${serviceUrl}?plz=${zip}`
+    await wv.loadURL(url)
+
+    // html result parsing
+    let html = await wv.getHTML()
+    // maintenance mode?
+    if (html.includes('Wartungsarbeiten')) {
+      return { state: 'maintenance' }
+    } else if (html.includes('Warteraum')) { // virtual waiting room?
+      return { state: 'waitingRoom' }
+    }
+
+    let lstMs = await new Request(vaccineListUrl).loadJSON()
+    lstMString = lstMs.map(item => item.qualification).join(',')
+
+    // request slots available
+    await wv.evaluateJavaScript('setTimeout(function(){completion(null)}, 3000)', true)
+    await wv.loadURL(`${requestSlotUrl}?plz=${zip}&leistungsmerkmale=${lstMString}`)
+    html = await wv.getHTML()
+
+    // remove tags
+    let htmlRes = html.replace(/(<([^>]+)>)/gi, '')
+
+    jsonResponse = { state: 'ok', res: null }
+    jsonResponse.res = JSON.parse(htmlRes)
+
+  } catch (e) {
+    return { state: 'error', message: e.message }
+  }
+
+  return jsonResponse
+
 }
 
 const createWidget = async () => {
 
-  let lastCheck = null
-  let hasSlots = false
+  let hasSlots = null
   let storedData = getStoredData()
   let zip = args.widgetParameter || 48155
-  let debug = ''
 
-  if (storedData && zip) {
-    lastCheck = await getData(zip)
-    debug = JSON.stringify(lastCheck)
-    hasSlots = lastCheck.termineVorhanden
+  let dataRes = await getData(zip)
+
+  if (dataRes.state === 'ok' && dataRes.res.hasOwnProperty('termineVorhanden')) {
+    hasSlots = dataRes.res.termineVorhanden
   }
 
   let widget = new ListWidget()
@@ -52,19 +81,21 @@ const createWidget = async () => {
   }
 
   widget.backgroundColor = hasSlots ? Color.green() : Color.red()
+
+  if (dataRes.state === 'maintenance' || dataRes.state === 'waitingRoom') {
+    widget.backgroundColor = Color.orange()
+  } else if (dataRes.state === 'error') {
+    widget.backgroundColor = Color.red()
+  }
+
   widget.url = `${serviceUrl}?plz=${zip}`
 
+  // notification if slots are now available
   if (hasSlots && !storedData.lastSuccess) {
-    let note = new Notification()
-    note.title = 'Impftermin-Service'
-    note.subtitle = 'Neue Termine verfügbar'
-    note.body = `Es sind Termine verfügbar für ${storedData.zip}.`
-    note.openURL = `${serviceUrl}?plz=${storedData.zip}`
-    note.sound = 'alert'
+    let note = createNotification(zip, widget.url)
     let scheduledDate = new Date(new Date().getTime() + 1000)
     note.setTriggerDate(scheduledDate)
     await note.schedule()
-    storedData.lastSuccess = true
   }
 
   storedData.lastSuccess = hasSlots
@@ -74,17 +105,22 @@ const createWidget = async () => {
   let stack = widget.addStack()
   stack.layoutVertically()
 
-  if (lastCheck !== null && hasSlots !== undefined) {
-    createText(stack, dateFormatter.string(new Date()), timeFont, 5)
-    createText(stack, 'Impftermin-Service')
-    createText(stack, 'PLZ: ' + storedData.zip, titleFont, 5)
+  createText(stack, dateFormatter.string(new Date()), timeFont, 5)
+  createText(stack, 'Impftermin-Service')
+
+  if (dataRes.state === 'maintenance') {
+    createText(stack, 'Wartungsarbeiten', titleFontB, 5)
+  } else if (dataRes.state === 'waitingRoom') {
+    createText(stack, 'Warteraum', titleFontB, 5)
+  } else if (hasSlots !== null) {
+    createText(stack, 'PLZ: ' + zip, titleFontB, 5)
     createText(stack, hasSlots ? 'Termine vorhanden' : 'Keine Termine vorhanden', responseFont, 5)
     if (hasSlots) {
       createText(stack, 'Jetzt Termine buchen!', titleFontB, 0)
     }
   } else {
-    createText(stack, 'Verfügbarkeit konnte nicht geprüft werden. ', timeFont)
-    createText(stack, 'Antwort: ' + debug, timeFont)
+    createText(stack, 'Verfügbarkeit konnte nicht geprüft werden. ', titleFontB, 5)
+    createText(stack, 'Antwort: ' + dataRes.state === 'error' ? dataRes.message : JSON.stringify(dataRes), timeFont, 5)
   }
 
   return widget
@@ -109,10 +145,14 @@ const getStoredData = () => {
   return Keychain.contains('storedData') ? JSON.parse(Keychain.get('storedData')) : { lastSuccess: false }
 }
 
-const clearStoredData = () => {
-  if (Keychain.contains('storedData')) {
-    Keychain.remove('storedData')
-  }
+const createNotification = (zip, url) => {
+  let note = new Notification()
+  note.title = 'Impftermin-Service'
+  note.subtitle = 'Neue Termine verfügbar'
+  note.body = `Es sind Termine verfügbar für ${zip}.`
+  note.openURL = url
+  note.sound = 'alert'
+  return note
 }
 
 const run = async () => {
